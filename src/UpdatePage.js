0932809a484a2127b4b3d6d635b4504c1053cd18
JeffAllison <itsjallison@gmail.com>
2022-05-11 16:39:08 -0700

    copy pasted create page into update page, changed necessary verbiage to reflect UPDATE, imported useparams, imported getGamebyID and updateGame from fetch utils, added useEffect to pull in created data into the form inputs

--- src/UpdatePage.js
+++ src/UpdatePage.js
@@ -1,20 +1,137 @@
-import { useEffect, useState } from 'react';
-import { useRouteMatch } from 'react-router-dom';
-import { getGameById } from './services/fetch-utils';
+import React, { useState } from 'react';
+import { getGameById, updateGame } from './services/fetch-utils';
+import { useHistory, useParams } from 'react-router-dom';
+import { useEffect } from 'react/cjs/react.production.min';
 
 export default function UpdatePage() {
-  const [game, setGame] = useState({});
+  // you'll need the history hook from react-router-dom to do your redirecting in the handleSubmit
+  const { push } = useHistory();
+  const { id } = useParams();
+  const [gameInTheForm, setGameInTheForm] = useState({
+    title: '',
+    genre: '',
+    designer: '',
+    description: '',
+    minPlayers: 0,
+    maxPlayers: 0,
+  });
 
-  // on mount, fetch and set in state the correct board game for this id (the id can be found in match.params using the correct react-router hook)
+  useEffect(() => {
+    async function load() {
+      const game = await getGameById(id);
+
+      setGameInTheForm(game);
+    }
+    load();
+  }, [id]);
+
+  async function handleUpdateSubmit(e) {
+    e.preventDefault();
+
+    await updateGame(id, gameInTheForm);
+
+    // use history.push to send the user to the list page
+    push('/board-games');
+  }
 
   return (
-    <div className="detail">
-      <h1>{game.title}</h1>
-      <h2>
-        A {game.genre} game for {game.min_players} to {game.max_players} players
-      </h2>
-      <h3>by celebrated designer {game.designer}</h3>
-      <p>{game.description}</p>
+    <div className="update">
+      {/* on submit, call your handleSubmit function */}
+      <form onSubmit={handleUpdateSubmit}>
+        <h2>Add board game</h2>
+        <label>
+          Title
+          {/* on change, set the title in state */}
+          <input
+            value={gameInTheForm.title}
+            type="text"
+            onChange={(e) =>
+              setGameInTheForm({
+                ...gameInTheForm,
+                title: e.target.value,
+              })
+            }
+          />
+        </label>
+        <label>
+          Genre
+          {/* on change, set the genre in state */}
+          <select
+            onChange={(e) =>
+              setGameInTheForm({
+                ...gameInTheForm,
+                genre: e.target.value,
+              })
+            }
+            value={gameInTheForm.genre}
+          >
+            <option>Tile-laying</option>
+            <option>Economic</option>
+            <option>War</option>
+            <option>Card</option>
+            <option>Abstract</option>
+            <option>Cooperative</option>
+            <option>Solo</option>
+          </select>
+        </label>
+        <label>
+          Designer
+          {/* on change, set the designer in state */}
+          <input
+            value={gameInTheForm.designer}
+            type="text"
+            onChange={(e) =>
+              setGameInTheForm({
+                ...gameInTheForm,
+                designer: e.target.value,
+              })
+            }
+          />
+        </label>
+        <label>
+          Min Players
+          {/* on change, set the min players in state */}
+          <input
+            value={gameInTheForm.min_players}
+            type="number"
+            onChange={(e) =>
+              setGameInTheForm({
+                ...gameInTheForm,
+                min_players: e.target.value,
+              })
+            }
+          />
+        </label>
+        <label>
+          Max Players
+          {/* on change, set the max players in state */}
+          <input
+            value={gameInTheForm.max_players}
+            type="number"
+            onChange={(e) =>
+              setGameInTheForm({
+                ...gameInTheForm,
+                max_players: e.target.value,
+              })
+            }
+          />
+        </label>
+        <label>
+          Description
+          {/* on change, set the description in state */}
+          <input
+            value={gameInTheForm.description}
+            type="text"
+            onChange={(e) =>
+              setGameInTheForm({
+                ...gameInTheForm,
+                description: e.target.value,
+              })
+            }
+          />
+        </label>
+        <button>Update game</button>
+      </form>
     </div>
   );
 }
