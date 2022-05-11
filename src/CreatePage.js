79575d43285d7dffb9b2931129d54f84380c912b
JeffAllison <itsjallison@gmail.com>
2022-05-11 16:26:43 -0700

    set set for use history and gameinform in createpage.js, added on change functions to SB row value inputs on createpage.js

--- src/CreatePage.js
+++ src/CreatePage.js
@@ -1,39 +1,59 @@
 import { useState } from 'react';
 import { createGame } from './services/fetch-utils';
+import { useHistory } from 'react-router-dom';
 
 export default function CreatePage() {
   // you'll need the history hook from react-router-dom to do your redirecting in the handleSubmit
-
-  // here's the state you'll need:
-    // title;
-    // genre;
-    // designer;
-    // description;
-    // minPlayers;
-    // maxPlayers;
+  const history = useHistory();
+  const [gameInTheForm, setGameInTheForm] = useState({
+    title: '',
+    genre: '',
+    designer: '',
+    description: '',
+    minPlayers: 0,
+    maxPlayers: 0,
+  });
 
   async function handleSubmit(e) {
     e.preventDefault();
 
-    // create a game
+    await createGame(setGameInTheForm);
 
     // use history.push to send the user to the list page
+    history.push('/board-games');
   }
 
   return (
-    <div className='create'>
+    <div className="create">
       {/* on submit, call your handleSubmit function */}
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Add board game</h2>
         <label>
-            Title
+          Title
           {/* on change, set the title in state */}
-          <input required name='title' />
+          <input
+            value={gameInTheForm.title}
+            type="text"
+            onChange={(e) =>
+              setGameInTheForm({
+                ...gameInTheForm,
+                title: e.target.value,
+              })
+            }
+          />
         </label>
         <label>
-            Genre
+          Genre
           {/* on change, set the genre in state */}
-          <select required>
+          <select
+            onChange={(e) =>
+              setGameInTheForm({
+                ...gameInTheForm,
+                genre: e.target.value,
+              })
+            }
+            value={gameInTheForm.genre}
+          >
             <option>Tile-laying</option>
             <option>Economic</option>
             <option>War</option>
@@ -44,24 +64,60 @@ export default function CreatePage() {
           </select>
         </label>
         <label>
-            Designer
+          Designer
           {/* on change, set the designer in state */}
-          <input required name='designer' />
+          <input
+            value={gameInTheForm.designer}
+            type="text"
+            onChange={(e) =>
+              setGameInTheForm({
+                ...gameInTheForm,
+                designer: e.target.value,
+              })
+            }
+          />
         </label>
         <label>
-            Min Players
+          Min Players
           {/* on change, set the min players in state */}
-          <input required name='min_players' />
+          <input
+            value={gameInTheForm.min_players}
+            type="number"
+            onChange={(e) =>
+              setGameInTheForm({
+                ...gameInTheForm,
+                min_players: e.target.value,
+              })
+            }
+          />
         </label>
         <label>
-            Max Players
+          Max Players
           {/* on change, set the max players in state */}
-          <input required name='max_players' />
+          <input
+            value={gameInTheForm.max_players}
+            type="number"
+            onChange={(e) =>
+              setGameInTheForm({
+                ...gameInTheForm,
+                max_players: e.target.value,
+              })
+            }
+          />
         </label>
         <label>
-            Description
+          Description
           {/* on change, set the description in state */}
-          <textarea required name='max_players' />
+          <input
+            value={gameInTheForm.description}
+            type="text"
+            onChange={(e) =>
+              setGameInTheForm({
+                ...gameInTheForm,
+                description: e.target.value,
+              })
+            }
+          />
         </label>
         <button>Create game</button>
       </form>
