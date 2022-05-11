518a7da73d156ae021549f5cd62d8383f0a5e6a7
JeffAllison <itsjallison@gmail.com>
2022-05-11 15:32:33 -0700

    added exact path routes for each component page and token terneries to navigate between pages, added navlinks for each component page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import { useState, useEffect } from 'react';
-import { getUser } from './services/fetch-utils';
+import { getUser, logout } from './services/fetch-utils';
 import { BrowserRouter as Router, Switch, NavLink, Route, Redirect } from 'react-router-dom';
 import AuthPage from './AuthPage';
-import DetailPage from './DetailPage';
+import UpdatePage from './UpdatePage';
 import ListPage from './ListPage';
 import CreatePage from './CreatePage';
-
 import './App.css';
-import { logout } from './services/fetch-utils';
 
 export default function App() {
   // You'll need to track the user in state
   const [email, setEmail] = useState('');
-  const [token, setToken] = useState(getUser().access_token);
+  const [token, setToken] = useState('');
 
   // add a useEffect to get the user and inject the user object into state on load
   useEffect(() => {
@@ -35,28 +33,69 @@ export default function App() {
   return (
     <Router>
       <div className="App">
-        <header>
+        <header className="links">
           {/* if there is a user in state, render out a link to the board games list, the create page, and add a button to let the user logout */}
-          <li>
-            <NavLink to="/">Home Page</NavLink>
-            <p>{email}</p>
-            <p>{token}</p>
-            <button onClick={handleLogout}>LogOut</button>
-          </li>
+          <ul>
+            <li>
+              <NavLink
+                to="/"
+                className={(isActive) => 'nav-link' + (!isActive ? ' unselected' : '')}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/create"
+                className={(isActive) => 'nav-link' + (!isActive ? ' unselected' : '')}
+              >
+                Create
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/board-games/:id"
+                className={(isActive) => 'nav-link' + (!isActive ? ' unselected' : '')}
+              >
+                Update
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/board-games"
+                className={(isActive) => 'nav-link' + (!isActive ? ' unselected' : '')}
+              >
+                List{' '}
+              </NavLink>
+            </li>
+            <li>
+              <p>{email}</p>
+              <p>{token}</p>
+              <button onClick={handleLogout}>LogOut</button>
+            </li>
+          </ul>
         </header>
         <main>
           <Switch>
             <Route exact path="/">
               {/* if there is a user, redirect to the board games list. Otherwise, render the auth page. Note that the AuthPage will need a function called setUser that can set the user state in App.js */}
+              {token ? (
+                <Redirect to="/board-games" />
+              ) : (
+                <AuthPage setEmail={setEmail} setToken={setToken} />
+              )}
             </Route>
             <Route exact path="/board-games">
               {/* if there is a user, render the board games list. Otherwise, redirect to the home route/auth page */}
+              {token ? <ListPage /> : <Redirect to="/" />}
             </Route>
             <Route exact path="/board-games/:id">
               {/* if there is a user, render the detail page. Otherwise, redirect to the home route/auth page */}
+              {token ? <UpdatePage /> : <Redirect to="/" />}
             </Route>
             <Route exact path="/create">
               {/* if there is a user, render the create page. Otherwise, redirect to the home route/auth page */}
+              {token ? <CreatePage /> : <Redirect to="/" />}
             </Route>
           </Switch>
         </main>
