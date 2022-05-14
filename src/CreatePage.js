24e8f5a82e1e9c987dd61438d8b926a81eb5deed
JeffAllison <itsjallison@gmail.com>
2022-05-13 17:04:23 -0700

    set class component state with imported WithRouter component, set state within that component, and called class component state in handleSubmit, added identifier this to handle submit in form and set new class component state in input on change components in createpage.js

--- src/CreatePage.js
+++ src/CreatePage.js
@@ -1,132 +1,139 @@
-import { useState } from 'react';
+//import { useState } from 'react';
+import React from 'react';
 import { createGame } from './services/fetch-utils';
-import { useHistory } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
-export default function CreatePage() {
-  // you'll need the history hook from react-router-dom to do your redirecting in the handleSubmit
-  const history = useHistory();
-  const [gameInTheForm, setGameInTheForm] = useState({
-    title: '',
-    genre: '',
-    designer: '',
-    description: '',
-    min_players: 0,
-    max_players: 0,
-  });
+// you'll need the history hook from react-router-dom to do your redirecting in the handleSubmit
+export default withRouter(
+  class CreatePage extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        title: '',
+        genre: 'Tile-laying',
+        designer: '',
+        description: '',
+        min_players: 0,
+        max_players: 0,
+      };
+    }
 
-  async function handleSubmit(e) {
-    e.preventDefault();
+    handleSubmit = async (e) => {
+      e.preventDefault();
 
-    await createGame(gameInTheForm);
+      await createGame(this.state);
 
-    // use history.push to send the user to the list page
-    history.push('/board-games');
+      // use history.push to send the user to the list page
+      this.props.history.push('/board-games');
+    };
+
+    render() {
+      return (
+        <div className="create">
+          {/* on submit, call your handleSubmit function */}
+          <form onSubmit={this.handleSubmit}>
+            <h2>Add board game</h2>
+            <label>
+              Title
+              {/* on change, set the title in state */}
+              <input
+                value={this.state.title}
+                type="text"
+                onChange={(e) =>
+                  this.setState({
+                    ...this.state,
+                    title: e.target.value,
+                  })
+                }
+                name="title"
+              />
+            </label>
+            <label>
+              Genre
+              {/* on change, set the genre in state */}
+              <select
+                onChange={(e) =>
+                  this.setState({
+                    ...this.state,
+                    genre: e.target.value,
+                  })
+                }
+                value={this.state.genre}
+                name="genre"
+              >
+                <option>Tile-laying</option>
+                <option>Economic</option>
+                <option>War</option>
+                <option>Card</option>
+                <option>Abstract</option>
+                <option>Cooperative</option>
+                <option>Solo</option>
+              </select>
+            </label>
+            <label>
+              Designer
+              {/* on change, set the designer in state */}
+              <input
+                value={this.state.designer}
+                type="text"
+                onChange={(e) =>
+                  this.setState({
+                    ...this.state,
+                    designer: e.target.value,
+                  })
+                }
+                name="designer"
+              />
+            </label>
+            <label>
+              Min Players
+              {/* on change, set the min players in state */}
+              <input
+                value={this.state.min_players}
+                type="number"
+                onChange={(e) =>
+                  this.setState({
+                    ...this.state,
+                    min_players: e.target.value,
+                  })
+                }
+                name="min_players"
+              />
+            </label>
+            <label>
+              Max Players
+              {/* on change, set the max players in state */}
+              <input
+                value={this.state.max_players}
+                type="number"
+                onChange={(e) =>
+                  this.setState({
+                    ...this.state,
+                    max_players: e.target.value,
+                  })
+                }
+                name="max_players"
+              />
+            </label>
+            <label>
+              Description
+              {/* on change, set the description in state */}
+              <input
+                value={this.state.description}
+                type="text"
+                onChange={(e) =>
+                  this.setState({
+                    ...this.state,
+                    description: e.target.value,
+                  })
+                }
+                name="description"
+              />
+            </label>
+            <button>Create game</button>
+          </form>
+        </div>
+      );
+    }
   }
-
-  return (
-    <div className="create">
-      {/* on submit, call your handleSubmit function */}
-      <form onSubmit={handleSubmit}>
-        <h2>Add board game</h2>
-        <label>
-          Title
-          {/* on change, set the title in state */}
-          <input
-            value={gameInTheForm.title}
-            type="text"
-            onChange={(e) =>
-              setGameInTheForm({
-                ...gameInTheForm,
-                title: e.target.value,
-              })
-            }
-            name="title"
-          />
-        </label>
-        <label>
-          Genre
-          {/* on change, set the genre in state */}
-          <select
-            onChange={(e) =>
-              setGameInTheForm({
-                ...gameInTheForm,
-                genre: e.target.value,
-              })
-            }
-            value={gameInTheForm.genre}
-            name="genre"
-          >
-            <option>Tile-laying</option>
-            <option>Economic</option>
-            <option>War</option>
-            <option>Card</option>
-            <option>Abstract</option>
-            <option>Cooperative</option>
-            <option>Solo</option>
-          </select>
-        </label>
-        <label>
-          Designer
-          {/* on change, set the designer in state */}
-          <input
-            value={gameInTheForm.designer}
-            type="text"
-            onChange={(e) =>
-              setGameInTheForm({
-                ...gameInTheForm,
-                designer: e.target.value,
-              })
-            }
-            name="designer"
-          />
-        </label>
-        <label>
-          Min Players
-          {/* on change, set the min players in state */}
-          <input
-            value={gameInTheForm.min_players}
-            type="number"
-            onChange={(e) =>
-              setGameInTheForm({
-                ...gameInTheForm,
-                min_players: e.target.value,
-              })
-            }
-            name="min_players"
-          />
-        </label>
-        <label>
-          Max Players
-          {/* on change, set the max players in state */}
-          <input
-            value={gameInTheForm.max_players}
-            type="number"
-            onChange={(e) =>
-              setGameInTheForm({
-                ...gameInTheForm,
-                max_players: e.target.value,
-              })
-            }
-            name="max_players"
-          />
-        </label>
-        <label>
-          Description
-          {/* on change, set the description in state */}
-          <input
-            value={gameInTheForm.description}
-            type="text"
-            onChange={(e) =>
-              setGameInTheForm({
-                ...gameInTheForm,
-                description: e.target.value,
-              })
-            }
-            name="description"
-          />
-        </label>
-        <button>Create game</button>
-      </form>
-    </div>
-  );
-}
+);
--- src/UpdatePage.js
+++ src/UpdatePage.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { getGameById, updateGame } from './services/fetch-utils';
-import { useHistory, useParams } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 export default function UpdatePage() {
   // you'll need the history hook from react-router-dom to do your redirecting in the handleSubmit
